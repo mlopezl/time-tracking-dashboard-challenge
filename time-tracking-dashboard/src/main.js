5113ccd4b730a30ae24dad5c9d6332e40d8686c1
Maynor Lopez <maynor94l39@gmail.com>
2026-06-02 19:23:43 -0600

    Quick refactor for main.js creating renderActivities function to reuse code

--- time-tracking-dashboard/src/main.js
+++ time-tracking-dashboard/src/main.js
@@ -18,74 +18,44 @@ async function obtainData() {
     return null;
   }
 }
+
 results.innerHTML = "";
 data.forEach((category) => {
-    
-    const categoryTitle = category.title.toLowerCase().replaceAll(" ", "-");
-    const categoryWeekly = category.timeframes.weekly;
-    results.innerHTML += `<article class="activity bg-${categoryTitle} w-70 h-35 rounded-xl relative overflow-hidden flex flex-col justify-end gap-5 xl:w-60 xl:h-55">
-          <img class="w-15 absolute -top-1 right-3" src="./src/assets/images/icon-${categoryTitle}.svg" alt="work icon">
-          <div class="activity__data absolute -bottom-1  w-full h-27 flex flex-col gap-2 p-4 rounded-xl bg-navy-900 z-2 hover:filter hover:brightness-180 transition transition-all duration-500 xl:h-45 xl:p-5">
-            <div class="labels flex justify-between items-center text-white">
-              <p class="capitalize">${categoryTitle}</p>
-              <img class="w-5 h-1" src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon">
-            </div>
-            <div class="data flex justify-between items-center text-white xl:flex-col xl:items-start xl:gap-5">
-              <p class="text-3xl font-extralight xl:text-5xl xl:pt-3">${categoryWeekly.current}hrs</p>
-              <p class="text-xs text-navy-200">Last Week - ${categoryWeekly.previous}hrs</p>
-            </div>
-          </div>
-        </article>`;
-  });
+    renderActivities(category, 'weekly', 'Week');
+});
+
 
 dailyButton.addEventListener("click", () => {
   results.innerHTML = "";
+  const timeframe = 'daily';
+  const timeUnit = 'Day';
   data.forEach((category) => {
-    const categoryTitle = category.title.toLowerCase().replaceAll(" ", "-");
-    const categoryDaily = category.timeframes.daily;
-    results.innerHTML += `<article class="activity bg-${categoryTitle} w-70 h-35 rounded-xl relative overflow-hidden flex flex-col justify-end gap-5 xl:w-60 xl:h-55">
-          <img class="w-15 absolute -top-1 right-3" src="./src/assets/images/icon-${categoryTitle}.svg" alt="work icon">
-          <div class="activity__data absolute -bottom-1  w-full h-27 flex flex-col gap-2 p-4 rounded-xl bg-navy-900 z-2 hover:filter hover:brightness-180 hover:transition hover:transition-all hover:duration-500 xl:h-45 xl:p-5">
-            <div class="labels flex justify-between items-center text-white">
-              <p class="capitalize">${categoryTitle}</p>
-              <img class="w-5 h-1" src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon">
-            </div>
-            <div class="data flex justify-between items-center text-white xl:flex-col xl:items-start xl:gap-5">
-              <p class="text-3xl font-extralight xl:text-5xl xl:pt-3">${categoryDaily.current}hrs</p>
-              <p class="text-xs text-navy-200">Last Day - ${categoryDaily.previous}hrs</p>
-            </div>
-          </div>
-        </article>`;
+   return renderActivities(category, timeframe, timeUnit)
   });
 });
 
 weeklyButton.addEventListener("click", () => {
   results.innerHTML = "";
+  const timeframe = 'weekly';
+  const timeUnit = 'Week';
   data.forEach((category) => {
-    const categoryTitle = category.title.toLowerCase().replaceAll(" ", "-");
-    const categoryWeekly = category.timeframes.weekly;
-   results.innerHTML += `<article class="activity bg-${categoryTitle} w-70 h-35 rounded-xl relative overflow-hidden flex flex-col justify-end gap-5 xl:w-60 xl:h-55">
-          <img class="w-15 absolute -top-1 right-3" src="./src/assets/images/icon-${categoryTitle}.svg" alt="work icon">
-          <div class="activity__data absolute -bottom-1  w-full h-27 flex flex-col gap-2 p-4 rounded-xl bg-navy-900 z-2 hover:filter hover:brightness-180 hover:transition hover:transition-all hover:duration-500 xl:h-45 xl:p-5">
-            <div class="labels flex justify-between items-center text-white">
-              <p class="capitalize">${categoryTitle}</p>
-              <img class="w-5 h-1" src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon">
-            </div>
-            <div class="data flex justify-between items-center text-white xl:flex-col xl:items-start xl:gap-5">
-              <p class="text-3xl font-extralight xl:text-5xl xl:pt-3">${categoryWeekly.current}hrs</p>
-              <p class="text-xs text-navy-200">Last Week - ${categoryWeekly.previous}hrs</p>
-            </div>
-          </div>
-        </article>`;
+    renderActivities(category, timeframe, timeUnit)
   });
 });
 
 monthlyButton.addEventListener("click", () => {
   results.innerHTML = "";
+  const timeframe = 'monthly';
+  const timeUnit = 'Month';
   data.forEach((category) => {
+    renderActivities(category, timeframe, timeUnit);
+  });
+});
+
+function renderActivities(category, timeframe, timeUnit){
     const categoryTitle = category.title.toLowerCase().replaceAll(" ", "-");
-    const categoryMonthly = category.timeframes.monthly;
-   results.innerHTML += `<article class="activity bg-${categoryTitle} w-70 h-35 rounded-xl relative overflow-hidden flex flex-col justify-end gap-5 xl:w-60 xl:h-55">
+    const categoryTimeframe = category.timeframes[timeframe];
+    results.innerHTML += `<article class="activity bg-${categoryTitle} w-70 h-35 rounded-xl relative overflow-hidden flex flex-col justify-end gap-5 xl:w-60 xl:h-55">
           <img class="w-15 absolute -top-1 right-3" src="./src/assets/images/icon-${categoryTitle}.svg" alt="work icon">
           <div class="activity__data absolute -bottom-1  w-full h-27 flex flex-col gap-2 p-4 rounded-xl bg-navy-900 z-2 hover:filter hover:brightness-180 hover:transition hover:transition-all hover:duration-500 xl:h-45 xl:p-5">
             <div class="labels flex justify-between items-center text-white">
@@ -93,10 +63,9 @@ monthlyButton.addEventListener("click", () => {
               <img class="w-5 h-1" src="./src/assets/images/icon-ellipsis.svg" alt="Ellipsis icon">
             </div>
             <div class="data flex justify-between items-center text-white xl:flex-col xl:items-start xl:gap-5">
-              <p class="text-3xl font-extralight xl:text-5xl xl:pt-3">${categoryMonthly.current}hrs</p>
-              <p class="text-xs text-navy-200">Last Month - ${categoryMonthly.previous}hrs</p>
+              <p class="text-3xl font-extralight xl:text-5xl xl:pt-3">${categoryTimeframe.current}hrs</p>
+              <p class="text-xs text-navy-200">Last ${timeUnit} - ${categoryTimeframe.previous}hrs</p>
             </div>
           </div>
         </article>`;
-  });
-});
+}
